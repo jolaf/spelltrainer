ddfe5ff4bd2410e9fb0bad7a3c5a053cc254dc85
vmzakhar <vmzakhar@07fca57d-762d-06b4-be6e-a84d4e97fe9b>
2011-01-21 17:02:15 +0000

    v1.0 basic functionality complete

--- trunk/SpellTrainer.py
+++ trunk/SpellTrainer.py
@@ -7,15 +7,16 @@
 #
 # Uses TkInter for GUI: http://wiki.python.org/moin/TkInter
 #
-# Version 0.23
+# Version 1.0
 #
 
 # ToDo:
-# - Allow adjusting colors and fonts
+# - Allow specifying colors and fonts from command line options
 # - Make frequency of appearance depend on level
 # - Make delay depend on level
-# - Include Hogwarts hooligan spells
-# - Include support for fast series?
+# - Include Hogwarts hooligan spells (to train fast going in close quarters)
+# - Include some specialized spells
+# - Include support for fast series
 
 from base64 import b64decode
 from getopt import getopt
@@ -25,17 +26,15 @@ from sys import argv, exit # exit redefined # pylint: disable=W0622
 from tempfile import NamedTemporaryFile
 from thread import start_new_thread
 from time import sleep
-from traceback import format_exc
 
-from Tkinter import Frame, Label, StringVar, CENTER, FLAT, N, S, E, W
-from tkFont import Font
+from Tkinter import Frame, Label, StringVar, N, S, E, W
 
-TITLE = 'SpellTrainer v0.22'
+TITLE = 'SpellTrainer v1.0'
 
-SPELLS = ('Impedimenta', 'Tarantallegra', 'Silencio',			# Protego
-          'Rictusempra', 'Incarcero', 'Mento Menores',			# Defendo
-          'Stupefy', 'Achelitus', 'Maledicero',				# Enervate
-          'Incendio', 'Deluvium', 'Tormencio', 'Conjunctivitus')	# VIP
+SPELLS = ('Impedimenta', 'Tarantallegra', 'Silencio',		# Protego
+          'Rictusempra', 'Incarcero', 'Mento Menores',		# Defendo
+          'Stupefy', 'Achelitus', 'Maledicero',			# Enervate
+          'Incendio', 'Deluvium', 'Tormencio', 'Conjunctivitus')# VIP
 
 SIMPLA = SPELLS + ('Expelliarmus',)
 
@@ -43,23 +42,24 @@ MAXIMA = tuple(s + ' Maxima' for s in SPELLS) + ('Petrificus Totalus',)
 
 ULTIMA = tuple(s + ' Ultima' for s in SPELLS)
 
-ALLOW_REPEAT_ATTACK_AFTER = 2
+RULE_OF_N_SPELLS = 3
+
+MODE_ATTACK = 0
+MODE_DEFENCE = 1
+
+FONT_NAME = 'Verdana'
+FONT_SIZE = 10
+FONT_STYLE = 'bold'
+
+ATTACK_BACKGROUND  = 'black'
+ATTACK_FOREGROUND  = 'green'
+DEFENCE_BACKGROUND = 'red'
+DEFENCE_FOREGROUND = 'white'
+FLASH_BACKGROUND = 'white'
 
 class SpellTrainer(Frame): # pylint: disable=R0904
 
-    delay = 1
-
-    MODE_ATTACK = 0
-    MODE_DEFENCE = 1
-
-    FONT_NAME = 'Verdana'
-    FONT_SIZE = 10
-    FONT_STYLE = 'bold'
-
-    ATTACK_BACKGROUND  = 'black'
-    ATTACK_FOREGROUND  = 'green'
-    DEFENCE_BACKGROUND = 'red'
-    DEFENCE_FOREGROUND = 'white'
+    delay = 3
 
     def __init__(self, master = None):
         '''Parses command line parameters, initializes the GUI and starts the main loop.'''
@@ -89,9 +89,9 @@ class SpellTrainer(Frame): # pylint: disable=R0904
             spells += ULTIMA
         modes = []
         if 'a' in commands:
-            modes.append(self.MODE_ATTACK)
+            modes.append(MODE_ATTACK)
         if 'd' in commands:
-            modes.append(self.MODE_DEFENCE)
+            modes.append(MODE_DEFENCE)
         if not spells or not modes:
             usage("At least one of s/m/u and one of a/d commands must be specified")
         self.spells = tuple(spells)
@@ -99,40 +99,59 @@ class SpellTrainer(Frame): # pylint: disable=R0904
 
         # Initializing GUI
         Frame.__init__(self, master)
-        self.master.title(TITLE)
-        self.master.wm_geometry('235x100')
-        self.master.columnconfigure(0, weight = 1)
-        self.master.rowconfigure(0, weight = 1)
+        self.configureMaster(self.master)
+        self.configureFrame(self)
+        self.text = StringVar()
+        self.text.set(TITLE)
+        self.textAspectRatio = self.estimateTextAspectRatio(self.spells + (TITLE,))
+        self.label = self.createLabel(self, self.text)
+        self.master.bind('<Configure>', self.resize)
+        self.master.bind('<Key-Escape>', self.quit)
+
+        # Initializing GUI
+        start_new_thread(self.run, ()) # Start activity thread
+        self.mainloop()
+
+    @staticmethod
+    def configureMaster(master):
+        master.title(TITLE)
+        master.wm_geometry('235x100')
+        master.columnconfigure(0, weight = 1)
+        master.rowconfigure(0, weight = 1)
         try: # Setting window icon
             icoFile = NamedTemporaryFile(delete = False)
             icoFile.write(b64decode('AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAAAAAABMLAAATCwAAEAAAAAAAAAA3RkwA6OblAACw9wCLjY0AxszMAK+urQAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAZmZmZmZmZmZnd3d3d3d3dmd3d3d3d3d2Z3d3d3d3dQZnd0EDF3d2dmdAQCB3dwN2ZwdxAFd0Z3ZlF3dXd3Z3dmV3d3d3A3d2Yxd3d3Rnd3ZkB3d3dnd3dmdjd3cFd3d2Z3YAAEd3d3Znd3EXd3d3dmd3d3d3d3d2ZmZmZmZmZmYAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'))
             icoFile.close()
-            self.master.wm_iconbitmap(default = icoFile.name)
+            master.wm_iconbitmap(default = icoFile.name)
             remove(icoFile.name)
         except:
             pass # If we can't install icon for some reason, ok with that
-        self.grid(sticky = N+S+E+W)
-        self.rowconfigure(0, weight = 1)
-        self.columnconfigure(0, weight = 1)
-        self.text = StringVar()
-        self.text.set(TITLE)
-        self.label = Label(self, textvariable = self.text,
-                           anchor = CENTER, justify = CENTER, relief = FLAT, borderwidth = 0,
-                           font = (self.FONT_NAME, self.FONT_SIZE, self.FONT_STYLE))
-        self.label.grid(row = 0, column = 0, sticky = N+S+E+W)
-        self.master.bind('<Configure>', self.resize)
-        self.master.bind('<Key-Escape>', self.quit)
-        start_new_thread(self.run, ()) # Start activity thread
-        self.mainloop()
 
+    @staticmethod
+    def configureFrame(frame):
+        frame.grid(sticky = N+S+E+W)
+        frame.rowconfigure(0, weight = 1)
+        frame.columnconfigure(0, weight = 1)
+
+    @staticmethod
+    def createLabel(master, text):
+        label = Label(master, textvariable = text, font = (FONT_NAME, FONT_SIZE, FONT_STYLE))
+        label.grid(row = 0, column = 0, sticky = N+S+E+W)
+        return label
+
+    @staticmethod
+    def estimateTextAspectRatio(strings):
+        text = StringVar()
+        label = Label(None, font = (FONT_NAME, -1000, FONT_STYLE), height = 1, textvariable = text)
+        ret = 1000
+        for s in strings:
+            text.set(s)
+            ret = min(ret, float(label.winfo_reqheight()) / label.winfo_reqwidth())
+        return 0.78 * ret
+    
     def resize(self, event = None): # event is unused # pylint: disable=W0613
         '''Called when window size or contents changes.'''
-        # ToDo: find the longest spell name and adjust the font size for it instead of current spell
-        # ToDo: create Label statically
-        # ToDo: maybe calculate font proportion once and store THAT statically
-        t = Label(self, font = (self.FONT_NAME, -1000, self.FONT_STYLE), height = 1, text = self.text.get())
-        height = int(float(self.winfo_width()) * t.winfo_reqheight() / t.winfo_reqwidth())
-        self.label.configure(font = Font(family = self.FONT_NAME, size = -int(min(0.5 * self.winfo_height(), 0.78 * height)), weight = self.FONT_STYLE))
+        self.label.configure(font = (FONT_NAME, -int(min(0.5 * self.winfo_height(), self.textAspectRatio * self.winfo_width())), FONT_STYLE))
 
     def quit(self, event = None): # event is unused # pylint: disable=W0221, W0613
         '''Overrides standard method to allow using as event handler.'''
@@ -146,23 +165,25 @@ class SpellTrainer(Frame): # pylint: disable=R0904
         while True:
             try:
                 sleep(self.delay)
+                self.label.configure(background = FLASH_BACKGROUND, foreground = FLASH_BACKGROUND)
+                sleep(0.1)
                 mode = choice(self.modes)
                 spell = choice(self.spells)
-                if mode == self.MODE_ATTACK:
-                    while spell in previousSpells:
+                if mode == MODE_ATTACK:
+                    baseSpell = spell.partition(' ')[0]
+                    while baseSpell in previousSpells:
                         spell = choice(self.spells)
-                    # Improve to count any level of a spell in
-                    previousSpells = (previousSpells + [spell])[-ALLOW_REPEAT_ATTACK_AFTER:]
-                    self.label.configure(background = self.ATTACK_BACKGROUND, foreground = self.ATTACK_FOREGROUND)
-                elif mode == self.MODE_DEFENCE:
-                    self.label.configure(background = self.DEFENCE_BACKGROUND, foreground = self.DEFENCE_FOREGROUND)
+                        baseSpell = spell.partition(' ')[0]
+                    previousSpells = (previousSpells + [baseSpell])[-(RULE_OF_N_SPELLS - 1):]
+                    self.label.configure(background = ATTACK_BACKGROUND, foreground = ATTACK_FOREGROUND)
+                elif mode == MODE_DEFENCE:
+                    self.label.configure(background = DEFENCE_BACKGROUND, foreground = DEFENCE_FOREGROUND)
                 else:
                     assert False # this should never happen
                 self.text.set(spell)
                 self.resize()
             except:
-                print format_exc()
-                #pass # Sometimes exceptions fall out of Tkinter, just ignore them
+                pass # Sometimes exceptions fall out of Tkinter, just ignore them
 
 def usage(error = None):
     '''Prints usage information (preceded by optional error message) and exits with code 2.'''
